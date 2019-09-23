73600fd93d23462116f17b4792e820ddebee7be4
xhoneybee23 <jesuslim23@gmail.com>
2019-09-23 19:13:06 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+long long d[200001][4];
+
+long long go(int n, int m){
+	if (d[n][m] >= 0) return d[n][m];
+
+	//d[n][m] = 0;
+
+	long long temp;
+
+	if (m == 1) temp = (go(n - 1, 2) + go(n - 1, 3)) % 1000000009;
+	if (m == 2) temp = (go(n - 2, 1) + go(n - 2, 3))% 1000000009;
+	if (m == 3) temp = (go(n - 3, 1) + go(n - 3, 2)) % 1000000009;
+
+	d[n][m] = temp;
+
+	return d[n][m];
+}
+
+int main() {
+	int n;
+	cin >> n;
+
+	memset(d, -1, sizeof(d));
+
+	d[1][1] = 1; d[1][2] = 0; d[1][3] = 0;
+	d[2][2] = 1; d[2][1] = 0; d[2][3] = 0;
+	d[3][1] = 1; d[3][2] = 1; d[3][3] = 1;
+
+	while (n--){
+		long long input;
+		cin >> input;
+		long long result = 0;
+		for (int i = 1; i <= 3; i++){
+			result += (go(input, i));
+		}
+
+		result %= 1000000009;
+
+		cout << result << endl;
+	}
+
+	return 0;
+}
