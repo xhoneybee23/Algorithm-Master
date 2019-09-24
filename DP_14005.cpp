0d0f88e837d20b9ec337e89035fb417931925f13
xhoneybee23 <jesuslim23@gmail.com>
2019-09-24 22:44:39 +0900

    Update and rename main.cpp to DP_14005.cpp

--- DP_14005.cpp
+++ DP_14005.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+// LIS (Longest Increasing Subsequence) Problem Extended
+// LIS 의 길이와 해당 부분 수열도 출력
+// 부분 수열 출력 위해 이전 노드 인덱스를 저장하는 p 배열을 선언하고 go 함수로 인덱스 트랙킹하며 부분 수열 찾음
+// 부분 수열은 f에 저장해둠
+
 int a[1001];
 int t[1001];
 int p[1001];
