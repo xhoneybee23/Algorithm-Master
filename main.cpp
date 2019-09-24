1f9ff1e045e344a1a2c3be0afaa4ea2f6b60455c
xhoneybee23 <jesuslim23@gmail.com>
2019-09-24 21:49:32 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int a[1001];
+int t[1001];
+
+int main(){
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++){
+		scanf_s("%d", &a[i]);
+	}
+
+	t[0] = 0;
+
+	for (int j = 1; j <= n; j++){
+		t[j] = 1;
+		for (int k = j - 1; k >= 1; k--){
+			if ((a[k] < a[j]) && (t[j] < t[k]+1)) t[j] = t[k] + 1;
+		}
+	}
+
+	int max = t[1];
+
+	for (int i = 2; i <= n; i++){
+		max = (t[i] > max) ? t[i] : max;
+	}
+
+	cout << max << endl;
+
+	return 0;
+
+}
