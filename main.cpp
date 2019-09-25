ca281b388f7a56ecb4e6ef24c4d0fe52b54d94d8
xhoneybee23 <jesuslim23@gmail.com>
2019-09-25 13:06:00 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int a[1001];
+int s[1001];
+
+int main() {
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++){
+		scanf_s("%d", &a[i]);
+	}
+
+	for (int j = 1; j <= n; j++){
+		s[j] = a[j];
+		for (int k = j - 1; k >= 1; k--){
+			if ((a[j] > a[k]) && (a[j] + s[k] > s[j])){
+				s[j] = a[j] + s[k];
+			}
+		}
+	}
+
+	int temp = s[1];
+	for (int i = 1; i <= n; i++){
+		temp = (s[i] > temp) ? s[i] : temp;
+	}
+	cout << temp << endl;
+
+	return 0;
+}
