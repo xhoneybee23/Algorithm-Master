57c07dd9d9022ab2a2f2cc75cb9cdde517fbbacd
xhoneybee23 <jesuslim23@gmail.com>
2019-09-25 13:12:33 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// bottom-up
+// LIS(Longest Increasing Subsequence) Problem 
+
+int a[1001];
+int t[1001];
+
+int main(){
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++){
+		scanf_s("%d", &a[n-i+1]);
+	}
+
+	t[0] = 0;
+
+	for (int j = 1; j <= n; j++){
+		t[j] = 1;
+		for (int k = j - 1; k >= 1; k--){
+			if ((a[k] < a[j]) && (t[j] < t[k] + 1)) t[j] = t[k] + 1;
+		}
+	}
+
+	int max = t[1];
+
+	for (int i = 2; i <= n; i++){
+		max = (t[i] > max) ? t[i] : max;
+	}
+
+	cout << max << endl;
+
+	return 0;
+
+}
