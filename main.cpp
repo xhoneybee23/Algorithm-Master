d35fabd472c8b3014b061faf8a544d60f69f0f62
xhoneybee23 <jesuslim23@gmail.com>
2019-09-26 20:40:47 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int d[100001];
+
+int go(int n){
+	if (d[n] > 0) return d[n];
+	if (n == 1) return 1;
+	if (n == 0) return 0;
+
+	int temp = 100001;
+
+	int i=(int)sqrt(n);
+
+	for ( ; i>=1; i--){
+		d[n - (i*i)] = go(n - (i*i));
+		temp = (d[n - (i*i)] < temp) ? d[n - (i*i)] : temp;
+	}
+
+	d[n] = temp + 1;
+	return d[n];
+}
+
+int main() {
+	int n;
+	cin >> n;
+
+	cout << go(n) << endl;
+
+	return 0;
+}
