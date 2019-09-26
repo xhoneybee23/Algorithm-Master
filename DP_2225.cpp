b02a7cd07a3b5b3c38441db7b95d8376ee2dc58b
xhoneybee23 <jesuslim23@gmail.com>
2019-09-26 21:02:30 +0900

    Create DP_2225.cpp

---
+++ DP_2225.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+long long d[201][201];
+const int mod = 1000000000LL;
+
+int go(int k, int n){
+	if (k == 1) return 1;
+	if (k == 0) return 0;
+	if (d[k][n] > 0) return d[k][n];
+
+	for (int x = 0; x <= n; x++){
+		d[k][n] += go(k - 1, n - x);
+	}
+
+	d[k][n] %= mod;
+
+	return d[k][n];
+}
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+
+	cout << go(k, n) << endl;
+	return 0;
+}
